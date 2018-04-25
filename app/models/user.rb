326a574647f3a806319e7eae9abf82638259ac2c
Christine <junkichunky@yahoo.com>
2018-04-24 21:09:15 -0700

    remove code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates :Content, presence: true    # Replace FILL_IN with the right code.
-  validates :user_id, presence: true    # Replace FILL_IN with the right code.
+
 end
